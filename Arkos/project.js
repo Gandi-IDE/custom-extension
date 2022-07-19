b513d3752d20fdb44686372420858aa34c8b0f84
Arkos123 <101853866+Arkos123@users.noreply.github.com>
2022-07-19 22:58:18 +0800

    Update project.js

--- Arkos/project.js
+++ Arkos/project.js
@@ -15,7 +15,7 @@ class ArkosExtensions {
     this._formatMessage = runtime.getFormatMessage({
       'zh-cn': {
         'ArkosExt.extensionName': 'Arkosの拓展',
-        'ArkosExt.info1': '🚶 字符串处理',
+        'ArkosExt.info1': '🚶 坐标&方向',
         'ArkosExt.info2': '🔠 字符串处理',
         'ArkosExt.info3': '🛠 实用积木',
         'ArkosExt.info4': '📄 信息获取',
@@ -128,7 +128,7 @@ class ArkosExtensions {
         'ArkosExt.extra': 'extra',
 
         'ArkosExt.colorToHex': 'get code of color[COLOR]',
-
+        
         'ArkosExt.info1': '🚶 Coordinate & Direction',
         'ArkosExt.info2': '🔠 String Processing',
         'ArkosExt.info3': '🛠 Utilities',
