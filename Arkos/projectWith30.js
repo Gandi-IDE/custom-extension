81512bfd041c887a22960ba40a3578a5523f8807
Arkos123 <101853866+Arkos123@users.noreply.github.com>
2022-07-29 23:36:44 +0800

    测试

--- Arkos/projectWith30.js
+++ Arkos/projectWith30.js
@@ -1557,8 +1557,8 @@ class ArkosExtensions {
 	//镜像造型
 	mirrorSprite(args, util){
 		util.target.runtime.renderer._allDrawables[util.target.drawableID]._skinScale[args.mirrorMethod] *= -1;
-		util.target.emit('EVENT_TARGET_VISUAL_CHANGE', util.target);
-                util.target.runtime.requestRedraw();
+		//util.target.emit('EVENT_TARGET_VISUAL_CHANGE', util.target);
+                //util.target.runtime.requestRedraw();
 	}
 	//清除镜像
 	clearMirror(args, util){
@@ -1566,8 +1566,8 @@ class ArkosExtensions {
 			util.target.runtime.renderer._allDrawables[util.target.drawableID]._skinScale[i]=
 			Math.abs(util.target.runtime.renderer._allDrawables[util.target.drawableID]._skinScale[i]);
 		}
-		util.target.emit('EVENT_TARGET_VISUAL_CHANGE', util.target);
-                util.target.runtime.requestRedraw();
+		//util.target.emit('EVENT_TARGET_VISUAL_CHANGE', util.target);
+                //util.target.runtime.requestRedraw();
 	}
 	//
 	//角色跨域操作
