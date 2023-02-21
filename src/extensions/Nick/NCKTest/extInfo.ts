691e3a3190d6c90c4d0d49b009a238e7ed702be2
nickhu <314484063@qq.com>
2023-02-21 21:48:56 +0800

    test

---
+++ src/extensions/Nick/NCKTest/extInfo.ts
@@ -0,0 +1,44 @@
+import zhCn from "./l10n/zh-cn.json";
+import en from "./l10n/en.json";
+import cover from "./assets/cover.jpg";
+import blockIcon from "./assets/icon.png";
+import ext from "./index";
+
+export const extensionNS = 'Nick';
+
+export const extensionId = `${extensionNS}.NCKTest`;
+
+const buildInfoL10n = (l10n) => {
+  const {name, desc, docURL} = l10n;
+  const infoL10n = {}
+  infoL10n[`${extensionId}.name`] = name;
+  infoL10n[`${extensionId}.desc`] = desc;
+  infoL10n[`${extensionId}.docURL`] = docURL;
+  return infoL10n;
+}
+
+const infoL10n = {
+  'zh-cn': buildInfoL10n(zhCn),
+  en: buildInfoL10n(en),
+}
+
+
+export default {
+  Extension: () => ext,
+  info: {
+    name: `${extensionId}.name`,
+    description: `${extensionId}.desc`,
+    extensionId: extensionId,
+    iconURL: cover,
+    insetIconURL: blockIcon,
+    featured: true,
+    disabled: false,
+    collaborator: '',
+    doc: `${extensionId}.docURL`,
+  },
+  l10n: infoL10n,
+}
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import CRExtTest from './extensions/CatRanger/CRExtTest/extInfo';
+import NCKTest from './extensions/Nick/NCKTest/extInfo';
 // COMPONENT IMPORTS
 
 export {
   	CRExtTest,
+	NCKTest,
 // COMPONENT EXPORTS
 };
