7a558b3654bee4eb60d0ba5d5e8153d077251331
nickhu <314484063@qq.com>
2023-07-03 22:28:14 +0800

    publish: BlockStyle

--- src/publish.ts
+++ src/publish.ts
@@ -1,5 +1,7 @@
 // for publish to gandi extension service
 import SimpleUtils from "./extensions/YUEN/SimpleUtils/extInfo";
+import BlockStyle from "./extensions/KYSTEAM/BlockStyle/extInfo";
 export default {
   SimpleUtils,
+  BlockStyle
 };
