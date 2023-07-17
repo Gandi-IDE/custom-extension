c2b8bb66f89de2ed381cab739ae3cf0fb694565f
fath11 <124476952+fath11@users.noreply.github.com>
2023-07-17 01:52:05 +0000

    log bye bye

--- Fath/iamsupersmart.js
+++ Fath/iamsupersmart.js
@@ -225,7 +225,6 @@ class Circletetik {
   noop () {}
   calc_point_x_on_circle () {
     var info = this.getInfo();
-    console.log(info);
     return info.blocks[3].arguments.X + info.blocks[3].arguments.RADIUS * Math.cos(info.blocks[3].arguments.ANGLE)
   }
   returnFalse  () {
