a6839842f8d70790ee73b04e012e7933770f93cb
fath11 <124476952+fath11@users.noreply.github.com>
2023-07-17 02:29:28 +0000

    welp

--- Fath/welp.js
+++ Fath/welp.js
@@ -228,7 +228,7 @@ class Circletetik {
     console.log(info.blocks[3].arguments.X);
     console.log(info.blocks[3].arguments.RADIUS);
     console.log(info.blocks[3].arguments.ANGLE);
-    return info.blocks[3].arguments.X.value + info.blocks[3].arguments.RADIUS.value * Math.cosinfo.blocks[3].arguments.ANGLE.value)
+    return info.blocks[3].arguments.X + info.blocks[3].arguments.RADIUS * Math.cosinfo.blocks[3].arguments.ANGLE)
   }
   returnFalse  () {
     return false
