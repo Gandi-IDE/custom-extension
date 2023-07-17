773a220878a04b6cb2000f55aa0dee331204146e
fath11 <124476952+fath11@users.noreply.github.com>
2023-07-17 02:15:23 +0000

    final test

--- Fath/finaltest.js
+++ Fath/finaltest.js
@@ -228,7 +228,7 @@ class Circletetik {
     console.log(info.blocks[3].arguments.X);
     console.log(info.blocks[3].arguments.RADIUS);
     console.log(info.blocks[3].arguments.ANGLE);
-    return info.blocks[3].arguments.X + info.blocks[3].arguments.RADIUS * Math.cos(info.blocks[3].arguments.ANGLE)
+    return args.X + args.RADIUS * Math.cos(args.ANGLE)
   }
   returnFalse  () {
     return false
