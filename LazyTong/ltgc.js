9f280082a1cade01aa48bf544e7b95b05bdcd916
LazyTong <138383867+lazytongtech@users.noreply.github.com>
2023-07-20 07:12:22 +0800

    更新帮助文档

--- LazyTong/ltgc.js
+++ LazyTong/ltgc.js
@@ -126,7 +126,7 @@ class LazyTongGestureControl {
   }
 
   openHelpDocs() {
-    window.open("https://lazytongtech.github.io/help?project=ltgc");
+    window.open("https://www.ccw.site/post/93d8f103-4163-489e-b1a3-dc6a194cb0d6");
   }
 
   detectSwipe(args) {
