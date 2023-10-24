26c9568a0b169ecb9dcad3317314515e77ee6f29
fath11 <124476952+fath11@users.noreply.github.com>
2023-10-24 10:34:31 +0800

    Update Morecomparison.js

--- Fath/Morecomparison.js
+++ Fath/Morecomparison.js
@@ -584,7 +584,7 @@ const formatMessage = Scratch.translate;
             },
             {
                 collaborator: 'Fath11@Cocrea',
-                collaboratorURL: 'https://cocrea.world/@Fath11'
+                collaboratorURL: 'https://github.com/fath11'
             },
         ]
     },
