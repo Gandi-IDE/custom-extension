bd0609fa84506e9ff78a79afdf561e8f144affc9
Nightre <nightscratchs@gmail.com>
2023-12-27 16:49:34 +0800

    Update lang.js

--- Nights/stats.js/src/lang.js
+++ Nights/stats.js/src/lang.js
@@ -9,11 +9,11 @@ export default {
     },
 
     en: {
-        'Stats.extensionName': '性能监控',
-        'Stats.openStats': '打开性能监控',
-        'Stats.closeStats': '关闭性能监控',
-        'Stats.showAll': '全部显示',
-        'Stats.showOne': '单个显示',
-        'Stats.setStyle': '设置样式为[STYLE]'
+        'Stats.extensionName': 'state',
+        'Stats.openStats': 'open state',
+        'Stats.closeStats': 'close state',
+        'Stats.showAll': 'show all',
+        'Stats.showOne': 'show one',
+        'Stats.setStyle': 'set style [STYLE]'
     },
 }
