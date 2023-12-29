d7cb35a02880d73ac56eec5c611d40282a7884dc
Nightre <nightscratchs@gmail.com>
2023-12-29 15:22:56 +0800

    少翻译了一个文件

--- Nights/stats.js/src/block-info.js
+++ Nights/stats.js/src/block-info.js
@@ -2,23 +2,23 @@ import { STYLEENUM } from "./stats"
 export default (formatMessage) => {
     return {
         id: 'stats',
-        name: formatMessage('Stats.extensionName'),
+        name: formatMessage('nights.stats.extensionName'),
         color1: '#4D7EB4',
         blocks: [
             {
                 opcode: 'openStats',
                 blockType: "command",
-                text: formatMessage('Stats.openStats'),
+                text: formatMessage('nights.stats.openStats'),
             },
             {
                 opcode: 'closeStats',
                 blockType: "command",
-                text: formatMessage('Stats.closeStats'),
+                text: formatMessage('nights.stats.closeStats'),
             },
             {
                 opcode: 'setStyle',
                 blockType: "command",
-                text: formatMessage('Stats.setStyle'),
+                text: formatMessage('nights.stats.setStyle'),
                 arguments: {
                     STYLE: {
                         type: "string",
@@ -32,11 +32,11 @@ export default (formatMessage) => {
             STYLE: {
                 items: [
                     {
-                        text: formatMessage('Stats.showOne'),
+                        text: formatMessage('nights.stats.showOne'),
                         value: STYLEENUM.ONE.toString()
                     },
                     {
-                        text: formatMessage('Stats.showAll'),
+                        text: formatMessage('nights.stats.showAll'),
                         value: STYLEENUM.ALL.toString()
                     }
                 ]
