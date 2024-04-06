eb4ab19103a5330b11c779876479b54fdd84eeec
云云 <152984665+yunyun2010@users.noreply.github.com>
2024-04-06 18:36:59 +0800

    Create fetch.js

---
+++ TW/fetch.js
@@ -0,0 +1,38 @@
+// Name: Fetch
+// ID: fetch
+// Description: Make requests to the broader internet.
+// License: MIT AND MPL-2.0
+
+/* generated l10n code */Scratch.translate.setup({"de":{"_Fetch":"Internetquests"},"nb":{"_Fetch":"Hent"},"zh-cn":{"_Fetch":"请求API"}});/* end generated l10n code */(function (Scratch) {
+  "use strict";
+
+  class Fetch {
+    getInfo() {
+      return {
+        id: "fetch",
+        name: Scratch.translate("Fetch"),
+        blocks: [
+          {
+            opcode: "get",
+            blockType: Scratch.BlockType.REPORTER,
+            text: "GET [URL]",
+            arguments: {
+              URL: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "https://extensions.turbowarp.org/hello.txt",
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    get(args) {
+      return Scratch.fetch(args.URL)
+        .then((r) => r.text())
+        .catch(() => "");
+    }
+  }
+
+  Scratch.extensions.register(new Fetch());
+})(Scratch);
