67560aeaa83b68d90dde67c79e31331de1016490
xiaochen004hao <158283804+xiaochen004hao@users.noreply.github.com>
2024-04-13 20:28:47 +0800

    Update OBJLoader.js

--- RenderTheWorld/OBJLoader.js
+++ RenderTheWorld/OBJLoader.js
@@ -13,7 +13,7 @@ import {
 	PointsMaterial,
 	Vector3,
 	Color
-} from "https://unpkg.com/three@0.162.0/build/three.module.js";
+} from "./three.js";
 
 // o object_name | g group_name
 const _object_pattern = /^[og]\s*(.+)?/;
