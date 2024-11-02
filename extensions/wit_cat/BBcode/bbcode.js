82ea379399ef6ab0345adf0c5684005b6cce30ea
wit_cat <little_start@outlook.com>
2024-11-02 11:48:55 +0800

    fix:修复参数导致的xss注入

--- extensions/wit_cat/BBcode/bbcode.js
+++ extensions/wit_cat/BBcode/bbcode.js
@@ -21,13 +21,15 @@ let runtime;
   let key;
   const { slice } = [];
 
-  _ESCAPE_RE = /[&<>"]/g;
+  _ESCAPE_RE = /[&<>"'`]/g;
 
   _ESCAPE_DICT = {
     '&': '&amp;',
     '<': '&lt;',
     '>': '&gt;',
     '"': '&quot;',
+    "'": '&apos;',
+    '`': '&grave;'
   };
 
   _URL_RE =
@@ -137,11 +139,11 @@ let runtime;
         let results;
         results = [];
         for (k in attributes) {
-          v = attributes[k];
+          v = this.escape(attributes[k]);
           results.push(`${k}="${v}"`);
         }
         return results;
-      })().join(' ');
+      }).bind(this)().join(' ');
     };
 
     return Renderer;
