2f8e8a4aedbde820be31f8917d9fd99fdc93e443
wit_cat <96361142+little-starts@users.noreply.github.com>
2024-12-31 17:02:53 +0800

    BBcode V2.0 (#292)
    fix:修复参数名称导致的xss

--- extensions/wit_cat/BBcode/bbcode.js
+++ extensions/wit_cat/BBcode/bbcode.js
@@ -21,13 +21,15 @@ let runtime;
   let key;
   const { slice } = [];
 
-  _ESCAPE_RE = /[&<>"]/g;
+  _ESCAPE_RE = /[&<>"'`]/g;
 
   _ESCAPE_DICT = {
     '&': '&amp;',
     '<': '&lt;',
     '>': '&gt;',
     '"': '&quot;',
+    "'": '&apos;',
+    '`': '&grave;'
   };
 
   _URL_RE =
@@ -137,11 +139,11 @@ let runtime;
         let results;
         results = [];
         for (k in attributes) {
-          v = attributes[k];
-          results.push(`${k}="${v}"`);
+          v = this.escape(attributes[k]);
+          results.push(`${this.escape(k)}="${v}"`);
         }
         return results;
-      })().join(' ');
+      }).bind(this)().join(' ');
     };
 
     return Renderer;
