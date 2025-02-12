a19f60d671bf1d8b1bbad5ed3b44dd45582dfb80
bviibvii <bvlvbvlv@qq.com>
2025-02-12 13:15:38 +0800

    上传主代码

--- extensions/YUEN/FeishuExtension/src/constants.js
+++
@@ -1,9 +0,0 @@
-import Cover from './assets/cover.png';
-import Icon from './assets/icon.png';
-
-export { Cover, Icon };
-
-export const extensionNS = 'YUEN';
-
-// eslint-disable-next-line no-undef
-export const extensionId = `${extensionNS}.feishu${DEPLOY_ENV && DEPLOY_ENV === 'pre' ? '1' : ''}`;
